09569eb6baf2b016646d3b24276c87ea664e6a52
Choi Hojo <ubermensch100326@gmail.com>
2024-04-23 03:25:00 +0900

    [INFRA] deploy #9: 백엔드 파이프라인 구축

---
+++ exec/backend-docker-build-push.sh
@@ -0,0 +1,14 @@
+#!/bin/bash 
+
+export TAG=0.0.1
+export REGISTRY=k10d106.p.ssafy.io
+
+export IMG_NAME=backend
+
+docker login $REGISTRY -u $USERNAME -p $PASSWORD
+
+docker build -t $REGISTRY/$IMG_NAME:$TAG ../backend
+
+docker push $REGISTRY/$IMG_NAME:$TAG
+
+docker system prune -a -f
