e698bd8157dba396bc63baf40ee0434161ab69d4
최민경 <kjjpyckjs@gmail.com>
2024-04-23 10:35:07 +0900

    [FE] setting #17: 프론트엔드 도메인 설정

---
+++ frontend/src/pages/ownerpage/index.tsx
@@ -0,0 +1,5 @@
+const OwnerPage = () => {
+  return <div>사장님입니당</div>;
+};
+
+export default OwnerPage;
---
+++ frontend/src/pages/reservation/index.tsx
@@ -0,0 +1,5 @@
+const ReservationPage = () => {
+  return <div>예약페이지입니당</div>;
+};
+
+export default ReservationPage;
