87843fbb3f24a72179fcecd354e2a428f1943f00
Danbi <silverwill39@gmail.com>
2024-04-24 10:39:26 +0900

    [FE] feat #25: HeaderLink 컴포넌트 구현
    - HeaderLink 컴포넌트 구현
    - tailwind 색상 config 지정

--- frontend/src/components/@common/Header/Header.tsx
+++ frontend/src/components/@common/Header/Header.tsx
@@ -1,4 +1,8 @@
+import { useLocation, useNavigate } from "react-router-dom";
+
 const Header = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
   return <div></div>;
 };
 
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,12 +2,28 @@
 export default {
   content: [
     "./index.html",
-		"./src/**/*.{js,ts,jsx,tsx}",
-		"./node_modules/flowbite/**/*.js",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/flowbite/**/*.js",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        MAIN_GREEN: "#186D41",
+        MAIN_YELLOW: "#FEE970",
+        MAIB_PINK: "#FF777E",
+        MAIN_RED: "#E64B4B",
+        SUB_GREEN_01: "#F4FBF5",
+        SUB_GREEN_02: "#E3F0E5",
+        SUB_YELLOW: "#FFF8D4",
+        SUB_PINK: "#FFC9CC",
+        SUB_RED: "#FDA3A3",
+        BLACK: "#404040",
+        SUB_BLACK: "#595959",
+        GRAY: "#FAFAFA",
+        UNIMPORTANT_TEXT_01: "#595959",
+        UNIMPORTANT_TEXT_02: "#C8C8C8",
+      },
+    },
   },
   plugins: [],
-}
-
+};
