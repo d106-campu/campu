73d3279879bda1f2d1ea73a96260b9476d4337f3
Danbi <silverwill39@gmail.com>
2024-04-24 10:53:52 +0900

    [FE] setting #25: 파일 상대 경로 지정

--- frontend/src/components/@common/Header/Header.tsx
+++ frontend/src/components/@common/Header/Header.tsx
@@ -1,9 +1,15 @@
+import logo from "@/assets/images/Campu_logo2.png";
 import { useLocation, useNavigate } from "react-router-dom";
 
 const Header = () => {
   const navigate = useNavigate();
   const location = useLocation();
-  return <div></div>;
+
+  return (
+    <div>
+      <img src="" />
+    </div>
+  );
 };
 
 export default Header;
