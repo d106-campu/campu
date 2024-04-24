d4d5d648e9b2074473b32c4155b3659db4a1705a
박단비 <silverwill39@gmail.com>
2024-04-25 01:02:33 +0900

    [FE] design #31: 알림창 UI 구현
        - 알림창 UI 구현
        - 애니메이션 적용

--- frontend/src/components/Alert/AlertLink .tsx
+++ frontend/src/components/Alert/AlertLink .tsx
@@ -6,35 +6,54 @@ const AlertLink = ({ hasAlert }: { hasAlert: boolean }) => {
   const toggleOpen = () => [setOpenAlert(!openAlert)];
 
   return (
-    <>
-      <div className="flex justify-center flex-grow relative cusor-pointer mx-1 px-4 py-2  hover:bg-SUB_GREEN_01 rounded-md">
+    <div className="relative">
+      <div className=" flex justify-center flex-grow relative cusor-pointer mx-1 px-4 py-2  hover:bg-SUB_GREEN_01 rounded-md">
         <div
-          className="flex items-center justify-center  text-sm  hover:text-MAIN_GREEN"
+          className="flex items-center justify-center text-sm  hover:text-MAIN_GREEN"
           onClick={toggleOpen}
         >
           알림
         </div>
         {hasAlert && (
-          <span className="absolute top-1 right-8 block h-2.5 w-2.5 rounded-full bg-MAIN_GREEN border-2 border-white"></span>
+          <span className="absolute top-1 right-9 block h-2 w-2 rounded-full bg-MAIN_GREEN border-1 border-white"></span>
         )}
       </div>
 
       {/* 알림 목록 부분 */}
       {openAlert && (
-        <div className="absolute top-full right-72 mt-2 w-80 border-2 bg-white shadow-lg rounded-md z-10 p-3">
-          <div className="absolute bg-inherit h-5 w-5 rotate-45 transform origin-bottom-right -translate-y-1/2 left-5 top-1 rounded border-t-2 border-l-2"></div>
-          <AlertMessage
-            nickname={"캐치캠핑핑핑"}
-            time={1}
-            campingSite={"캠프유캠푸 캠핑장"}
-            campingZone={"A구역(벚꽃 캠핑존) 10 "}
-            total={3}
-            schedule={"24.05.10(금) ~ 05.14 (화) · 4박"}
-          />
-          {/* 여기에 추가적인 알림 컴포넌트를 넣을 수 있습니다. */}
+        <div className="absolute top-full left-0 mt-4 w-72 border-2 bg-white shadow-lg rounded-xl z-10 animate-ShowUp">
+          <div className="relative rounded-xl pt-4">
+            {/* 삼각형 모양 */}
+            <div className="bg-white absolute h-6 w-6 rotate-45 transform origin-bottom-right -translate-y-1/2 left-10 top-1 rounded border-t-2 border-l-2" />
+            {/* 스크롤 컨테이너 */}
+            <div className="overflow-hidden rounded-lg">
+              <div className="px-3 max-h-80 overflow-auto">
+                <h5 className="px-2 pb-2 text-sm">
+                  <span className="text-MAIN_GREEN font-bold">3</span> 개의
+                  새로운 알림이 있습니다.
+                </h5>
+                <AlertMessage
+                  nickname={"캐치캠핑핑핑"}
+                  time={1}
+                  campingSite={"캠프유캠푸 캠핑장"}
+                  campingZone={"A구역(벚꽃 캠핑존) 10 "}
+                  total={3}
+                  schedule={"24.05.10(금) ~ 05.14 (화) · 4박"}
+                />
+                <AlertMessage
+                  nickname={"캐치캠핑핑핑"}
+                  time={1}
+                  campingSite={"캠프유캠푸 캠핑장"}
+                  campingZone={"A구역(벚꽃 캠핑존) 10 "}
+                  total={3}
+                  schedule={"24.05.10(금) ~ 05.14 (화) · 4박"}
+                />
+              </div>
+            </div>
+          </div>
         </div>
       )}
-    </>
+    </div>
   );
 };
 export default AlertLink;
--- frontend/src/components/Alert/AlertMessage.tsx
+++ frontend/src/components/Alert/AlertMessage.tsx
@@ -14,28 +14,34 @@ const AlertMessage = ({
   total,
   schedule,
 }: IAlertMessageProps) => {
-  return (
-    <div className="p-5 text-sm text-BLACK bg-SUB_GREEN_01 rounded-sm">
-      <>
-        <p className="text-UNIMPORTANT_TEXT_02 text-xs">{time}일 전</p>
-        <p>
-          {nickname}님이 기다리신
-          <span className="text-MAIN_GREEN"> {campingSite}</span>의
-          빈자리가나왔어요!
-        </p>
-        <p>지금 바로 예약 해보세요 😊</p>
-        <div className="text-xs py-2">
-          <p>
-            {campingSite} - {campingZone}
-          </p>
-          <p>
-            인원 {total}명 / {schedule}
-          </p>
-        </div>
 
-        <p>서둘러 주세요, 자리가 금방 차버릴 수 있어요!</p>
-        <p className="text-MAIN_GREEN">클릭 시 예약 페이지로 이동합니다.</p>
-      </>
+  // @TODO: 클릭 시 해당 예약 페이지로 이동 
+  return (
+    <div className="pb-3">
+      <div className="px-5 pb-5 pt-3 text-sm text-SUB_BLACK bg-SUB_GREEN_01 rounded-lg">
+        <>
+          <p className="text-UNIMPORTANT_TEXT_02 text-xs text-end pb-2 pt-0">
+            {time}일 전
+          </p>
+          <p>
+            {nickname}님이 기다리신
+            <span className="text-MAIN_GREEN font-bold"> {campingSite}</span>의
+            빈자리가 나왔어요! 지금 바로 예약 해보세요 😊
+          </p>
+          <div className="text-xs py-2 font-bold text-black">
+            <p>
+              {campingSite} - {campingZone}
+              <br />
+              인원 {total}명 / {schedule}
+            </p>
+          </div>
+
+          <p>서둘러 주세요, 자리가 금방 차버릴 수 있어요!</p>
+          <p className="text-MAIN_GREEN text-xs pt-1">
+            클릭 시 예약 페이지로 이동합니다.
+          </p>
+        </>
+      </div>
     </div>
   );
 };
