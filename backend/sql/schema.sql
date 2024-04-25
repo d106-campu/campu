48bc7de80c96286ba2835bff231cfd0b3299e62c
Choi Hojo <ubermensch100326@gmail.com>
2024-04-25 14:00:57 +0900

    [BE] docs #23: DB schema 수정

--- backend/sql/schema.sql
+++ backend/sql/schema.sql
@@ -2,6 +2,7 @@ DROP DATABASE IF EXISTS d106;
 CREATE DATABASE d106;
 USE d106;
 
+
 -- d106.authority definition
 
 CREATE TABLE `authority` (
@@ -133,9 +134,11 @@ CREATE TABLE `user` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '식별번호',
   `account` varchar(16) NOT NULL COMMENT '아이디',
   `password` varchar(72) NOT NULL COMMENT '비밀번호',
-  `nickname` varchar(16) NOT NULL COMMENT '닉네임',
+  `nickname` varchar(8) NOT NULL COMMENT '닉네임',
   `gender` char(1) DEFAULT NULL COMMENT '성별',
   `birthyear` char(4) DEFAULT NULL COMMENT '출생년도',
+  `profile_image_url` varchar(1024) DEFAULT NULL COMMENT '프로필 이미지 주소',
+  `tel` varchar(16) DEFAULT NULL COMMENT '전화번호',
   `delete_time` datetime DEFAULT NULL COMMENT '회원탈퇴 시간',
   `create_time` datetime DEFAULT current_timestamp() COMMENT '회원가입 시간',
   `update_time` datetime DEFAULT NULL COMMENT '정보수정 시간',
@@ -160,9 +163,11 @@ CREATE TABLE `campsite` (
   `sigungu_nm` varchar(64) DEFAULT NULL COMMENT '시군구',
   `addr1` varchar(512) DEFAULT NULL COMMENT '주소',
   `addr2` varchar(512) DEFAULT NULL COMMENT '주소상세',
+  `induty_list` varchar(128) DEFAULT NULL COMMENT '유형 리스트',
+  `thumbnail_image_url` varchar(1024) DEFAULT NULL COMMENT '대표 이미지 주소',
+  `map_image_url` varchar(1024) DEFAULT NULL COMMENT '배치 이미지 주소',
   `homepage` varchar(1024) DEFAULT NULL COMMENT '홈페이지',
   `sited_stnc` int(11) DEFAULT NULL COMMENT '사이트간 거리',
-  `map_image_url` varchar(1024) DEFAULT NULL COMMENT '배치도',
   `animal_cmg_cl` varchar(16) DEFAULT NULL COMMENT '애완동물출입',
   `hit` bigint(20) DEFAULT 0 COMMENT '조회수',
   `create_time` datetime DEFAULT current_timestamp() COMMENT '생성 시간',
@@ -199,24 +204,10 @@ CREATE TABLE `campsite_image` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='캠핑장 이미지';
 
 
--- d106.campsite_induty definition
-
-CREATE TABLE `campsite_induty` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '식별번호',
-  `campsite_id` bigint(20) NOT NULL COMMENT '캠핑장 식별번호',
-  `induty_id` bigint(20) NOT NULL COMMENT '유형 식별번호',
-  PRIMARY KEY (`id`),
-  KEY `campsite_induty_induty_FK` (`induty_id`),
-  KEY `campsite_induty_campsite_FK` (`campsite_id`),
-  CONSTRAINT `campsite_induty_campsite_FK` FOREIGN KEY (`campsite_id`) REFERENCES `campsite` (`id`),
-  CONSTRAINT `campsite_induty_induty_FK` FOREIGN KEY (`induty_id`) REFERENCES `induty` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='캠핑장 유형';
-
-
 -- d106.campsite_location definition
 
 CREATE TABLE `campsite_location` (
-  `campsite_id` bigint(20) NOT NULL COMMENT '캠핑장 식별번호',
+  `campsite_id` bigint(20) NOT NULL,
   `map_x` double DEFAULT NULL COMMENT '경도',
   `map_y` double DEFAULT NULL COMMENT '위도',
   PRIMARY KEY (`campsite_id`),
@@ -244,7 +235,7 @@ CREATE TABLE `room` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '식별번호',
   `campsite_id` bigint(20) NOT NULL COMMENT '캠핑장 식별번호',
   `name` varchar(50) NOT NULL COMMENT '방 이름',
-  `type` varchar(8) NOT NULL COMMENT '방 타입',
+  `induty` varchar(8) NOT NULL COMMENT '방 유형',
   `base_no` int(11) NOT NULL COMMENT '기본 인원',
   `max_no` int(11) NOT NULL COMMENT '최대 인원',
   `price` int(11) NOT NULL COMMENT '가격',
@@ -272,6 +263,20 @@ CREATE TABLE `room_image` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='캠핑장 방 이미지';
 
 
+-- d106.room_induty definition
+
+CREATE TABLE `room_induty` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '식별번호',
+  `room_id` bigint(20) NOT NULL,
+  `induty_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `room_induty_induty_FK` (`induty_id`),
+  KEY `room_induty_room_FK` (`room_id`),
+  CONSTRAINT `room_induty_induty_FK` FOREIGN KEY (`induty_id`) REFERENCES `induty` (`id`),
+  CONSTRAINT `room_induty_room_FK` FOREIGN KEY (`room_id`) REFERENCES `room` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='방 유형';
+
+
 -- d106.user_authority definition
 
 CREATE TABLE `user_authority` (
@@ -288,6 +293,7 @@ CREATE TABLE `user_authority` (
 
 CREATE TABLE `reservation` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '식별번호',
+  `user_id` bigint(20) NOT NULL COMMENT '회원 식별번호',
   `room_id` bigint(20) NOT NULL COMMENT '방 식별번호',
   `head_cnt` int(11) NOT NULL COMMENT '숙박 인원',
   `price` int(11) NOT NULL COMMENT '결제 금액',
@@ -297,7 +303,9 @@ CREATE TABLE `reservation` (
   `update_time` datetime DEFAULT NULL COMMENT '수정 시간',
   PRIMARY KEY (`id`),
   KEY `reservation_room_FK` (`room_id`),
-  CONSTRAINT `reservation_room_FK` FOREIGN KEY (`room_id`) REFERENCES `room` (`id`)
+  KEY `reservation_user_FK` (`user_id`),
+  CONSTRAINT `reservation_room_FK` FOREIGN KEY (`room_id`) REFERENCES `room` (`id`),
+  CONSTRAINT `reservation_user_FK` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='캠핑장 예약';
 
 
