3266f5a11cb0aaef83ee22ac15a8e7322db0876b
Danbi <silverwill39@gmail.com>
2024-04-26 01:52:35 +0900

    [FE] feat #40: Modal 컴포넌트 구현

--- frontend/src/components/@common/Modal/Modal.tsx
+++ frontend/src/components/@common/Modal/Modal.tsx
@@ -1,33 +1,63 @@
-// import { useState } from 'react';
+import { PropsWithChildren, useState } from "react";
+import { IoCloseOutline } from "react-icons/io5";
 
-// interface IModalProps {
-//   isOpen: boolean;
-//   onClose: () => void;
-//   children: React.ReactNode;
-// };
+interface IModalProps {
+  width: string;
+  contentBackgroundColor?: string;
+  isBackgroundColorDark?: boolean;
+  padding?: string;
+  rounded?: string;
+  margin?: string;
+  onClose: () => void;
+  children: React.ReactNode;
+}
 
-// const Modal = ({
-//   isOpen,
-//   onClose,
-//   children,
-// }: IModalProps) => {
-//   const [isRendering, setIsRendering] = useState<boolean>(true);
+const Modal = ({
+  width,
+  padding = "p-5",
+  rounded = "rounded-lg",
+  contentBackgroundColor = "bg-white",
+  isBackgroundColorDark = true,
+  margin,
+  onClose,
+  children,
+}: PropsWithChildren<IModalProps>) => {
+  const [isRendering, setIsRendering] = useState<boolean>(true);
 
-//   const handleClose = () => {
-//     setIsRendering(false);
+  const handleClose = () => {
+    setIsRendering(false);
 
-//     setTimeout(() => {
-//       onClose();
-//     }, 500);
-//   };
+    setTimeout(() => {
+      onClose();
+    }, 250);
+  };
 
-//   return (
-//     <>
-//       <div>
-        
-//       </div>
-//     </>
-//   )
-// }
+  return (
+    <>
+      {/* 전체 화면을 덮는 투명 혹은 반투명 배경 구성 */}
+      <div
+        className={`absolute w-full h-full top-0 left-0 right-0 bottom-0 
+        ${isBackgroundColorDark ? "bg-black/70" : "bg-white/50"}`}
+        onClick={handleClose}
+      />
+      {/* 모달창*/}
+      <div
+        className={`absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 
+        ${width} ${padding} ${margin} ${rounded} ${contentBackgroundColor} 
+        ${!isBackgroundColorDark && "text-white"} 
+        ${isRendering ? "animate-modalOpen" : "animate-modalClose"}
+        `}
+      >
+        {/* 닫기 버튼 */}
+        <div className=" flex justify-end cursor-pointer" onClick={handleClose}>
+          <IoCloseOutline color={contentBackgroundColor} size={25} />
+        </div>
 
-// export default Modal;
+        {/*모달 내용 */}
+        {children}
+      </div>
+    </>
+  );
+};
+
+export default Modal;
