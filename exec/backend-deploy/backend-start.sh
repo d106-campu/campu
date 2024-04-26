c4c9853ea67ba0546591b7097520fe875215e894
Choi Hojo <ubermensch100326@gmail.com>
2024-04-27 00:48:24 +0900

    [INFRA] deploy #51: 백엔드 이미지 태그 로직 수정

--- exec/backend-deploy/backend-start.sh
+++ exec/backend-deploy/backend-start.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+export TAG=${env.GIT_COMMIT}
+
+sed -i "/^TAG=/c\TAG=${TAG}" docker-compose.yml
+
 docker compose pull
+
 docker compose up -d
+
 docker system prune -a -f
--- exec/backend-docker-build-push.sh
+++ exec/backend-docker-build-push.sh
@@ -1,14 +1,15 @@
 #!/bin/bash 
 
-export TAG=0.0.1
 export REGISTRY=k10d106.p.ssafy.io
 
 export IMG_NAME=backend
 
-docker login $REGISTRY -u $USERNAME -p $PASSWORD
+export TAG=${env.GIT_COMMIT}
 
-docker buildx build -t $REGISTRY/$IMG_NAME:$TAG ../backend
+docker login ${REGISTRY} -u ${USERNAME} -p ${PASSWORD}
 
-docker push $REGISTRY/$IMG_NAME:$TAG
+docker buildx build -t ${REGISTRY}/${IMG_NAME}:${TAG} ../backend
+
+docker push ${REGISTRY}/${IMG_NAME}:${TAG}
 
 docker system prune -a -f
