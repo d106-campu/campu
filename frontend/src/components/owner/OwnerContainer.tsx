01d671593acb1b4437532449b927b1036ebc3f12
최민경 <kjjpyckjs@gmail.com>
2024-04-26 16:18:26 +0900

    [FE] feat #45: 관리자페이지 캠핑장 사진 등록 ui 구현

--- frontend/src/components/owner/OwnerContainer.tsx
+++ frontend/src/components/owner/OwnerContainer.tsx
@@ -1,16 +1,28 @@
+import { useSelector } from "react-redux";
+import OwnerManage from "./OwnerManage";
+import OwnerReservation from "./OwnerReservation";
+import OwnerReview from "./OwnerReview";
 import OwnerTabbar from "./OwnerTabbar";
+import { RootState } from "@/app/store";
 
 const OwnerContainer = () => {
+  const tab = useSelector((state: RootState) => state.ownerTab.tab);
+
   return (
     <>
       <div className="flex justify-center py-10">
-        <div className="w-[70%] bg-blue-100">
+        <div className="w-[70%]">
           <p className="font-bold text-xl">캠핑장 관리</p>
           <OwnerTabbar
             leftTab="나의 캠핑장"
             middleTab="예약 관리"
             rightTab="리뷰"
           />
+          <div className="w-full">
+            {tab === "내 캠핑장" ? <OwnerManage /> : null}
+            {tab === "예약 관리" ? <OwnerReservation /> : null}
+            {tab === "리뷰" ? <OwnerReview /> : null}
+          </div>
         </div>
       </div>
     </>
