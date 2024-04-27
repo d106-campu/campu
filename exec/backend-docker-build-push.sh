003371173f600bdfcb1d063e60f5d6a8a2cd06da
Choi Hojo <ubermensch100326@gmail.com>
2024-04-27 11:17:17 +0900

    [INFRA] fix #51: 백엔드 이미지 태그 로직 오류 수정

--- exec/backend-docker-build-push.sh
+++ exec/backend-docker-build-push.sh
@@ -8,6 +8,8 @@ export IMG_NAME=backend
 
 docker login ${REGISTRY} -u ${USERNAME} -p ${PASSWORD}
 
-docker buildx build ${REGISTRY}/${IMG_NAME}:${TAG} ../backend
+docker buildx build -t ${REGISTRY}/${IMG_NAME}:${TAG} ../backend
 
 docker push ${REGISTRY}/${IMG_NAME}:${TAG}
+
+docker system prune -a -f
