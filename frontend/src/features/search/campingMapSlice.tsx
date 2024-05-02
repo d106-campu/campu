30a3bb6e45b145d598d8fbb8b72fbbf94ff1a4b4
최민경 <kjjpyckjs@gmail.com>
2024-05-02 10:59:38 +0900

    [FE] feat #75: 캠핑장 조회 결과를 저장하는 redux 상태관리 구현

--- frontend/src/features/search/campingMapSlice.tsx
+++ frontend/src/features/search/campingMapSlice.tsx
@@ -1,1 +1,23 @@
-// 더미
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { ICampingGround } from "@/types/search";
+
+interface CampingState {
+  campingData: ICampingGround[];
+}
+
+const initialState: CampingState = {
+  campingData: [],
+};
+
+const campingMapSlice = createSlice({
+  name: "campingMap",
+  initialState,
+  reducers: {
+    addCampingData(state, action: PayloadAction<ICampingGround[]>) {
+      state.campingData = action.payload;
+    },
+  },
+});
+
+export const { addCampingData } = campingMapSlice.actions;
+export const campingMapReducer = campingMapSlice.reducer;
