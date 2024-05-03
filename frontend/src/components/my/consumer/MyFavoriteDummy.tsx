df9fa3f4e7c5eefda1e5bf28bb6f644c5544a971
이귀현 <dlrnlgus7660@naver.com>
2024-05-03 14:46:54 +0900

    [FE] feat #97: 내가 찜한 캠핑장 UI 구현

---
+++ frontend/src/components/my/consumer/MyFavoriteDummy.tsx
@@ -0,0 +1,89 @@
+import photo1 from "@/assets/images/dummyCamping.png";
+import photo2 from "@/assets/images/dummy/camping_spot_2.png";
+import photo3 from "@/assets/images/dummy/camping_spot_3.png";
+import photo4 from "@/assets/images/dummy/camping_spot_4.jpg";
+
+// 더미데이터
+export const myCampingDummyList = [
+  {
+    id: 1,
+    name: "주용용캠핑장",
+    image: photo4,
+    rating: 4.5,
+    price: "50,000",
+    description: "깔끔하고 분위기 좋은 신상 캠핑 숙소",
+    location: "경상북도 구미시",
+  },
+  {
+    id: 2,
+    name: "챔챔경캠핑장",
+    image: photo2,
+    rating: 4.8,
+    price: "40,000",
+    description: "깔끔하고 분위기 좋은 신상 캠핑 숙소",
+    location: "경상북도 김천시",
+  },
+  {
+    id: 3,
+    name: "단비비캠핑장",
+    image: photo3,
+    rating: 3.8,
+    price: "60,000",
+    description: "깔끔하고 분위기 좋은 신상 캠핑 숙소",
+    location: "경상북도 싸피시",
+  },
+  {
+    id: 4,
+    name: "커커현캠핑장",
+    image: photo4,
+    rating: 3.8,
+    price: "120,000",
+    description: "깔끔하고 분위기 좋은 신상 캠핑 숙소",
+    location: "전라북도 싸피시",
+  },
+  {
+    id: 5,
+    name: "호호조캠핑장",
+    image: photo1,
+    rating: 4.0,
+    price: "100,000",
+    description: "깔끔하고 분위기 좋은 신상 캠핑 숙소",
+    location: "경상남도 창원시",
+  },
+  {
+    id: 6,
+    name: "준준호캠핑장",
+    image: photo2,
+    rating: 4.1,
+    price: "85,000",
+    description: "깔끔하고 분위기 좋은 신상 캠핑 숙소",
+    location: "부산광역시",
+  },
+  {
+    id: 7,
+    name: "귀찮아캠핑장",
+    image: photo3,
+    rating: 4.1,
+    price: "85,000",
+    description: "깔끔하고 분위기 좋은 신상 캠핑 숙소",
+    location: "부산광역시",
+  },
+  {
+    id: 8,
+    name: "집갈래캠핑장",
+    image: photo4,
+    rating: 4.1,
+    price: "85,000",
+    description: "깔끔하고 분위기 좋은 신상 캠핑 숙소",
+    location: "부산광역시",
+  },
+  {
+    id: 9,
+    name: "마지막캠핑장",
+    image: photo4,
+    rating: 4.1,
+    price: "85,000",
+    description: "깔끔하고 분위기 좋은 신상 캠핑 숙소",
+    location: "부산광역시",
+  },
+];
