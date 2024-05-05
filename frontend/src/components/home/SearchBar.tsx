bce16dee902cc02f5ccccbb0c41006f9d360b94a
최민경 <kjjpyckjs@gmail.com>
2024-05-05 18:27:39 +0900

    [FE] feat #107: selectbox 인원 선택 ui 구현

--- frontend/src/components/home/SearchBar.tsx
+++ frontend/src/components/home/SearchBar.tsx
@@ -4,19 +4,35 @@ import { MdOutlinePersonOutline } from "react-icons/md";
 import { LuSearch } from "react-icons/lu";
 import SearchRegion from "@/components/@common/Search/SearchRegion";
 import { RegionList } from "@/components/@common/Search/RegionList";
+import { useState } from "react";
+import { AiOutlineMinusCircle, AiOutlinePlusCircle } from "react-icons/ai";
 
 const SearchBar = () => {
+  const [numberOfPeople, setNumberOfPeople] = useState(2);
+
+  const handleDecrease = () => {
+    if (numberOfPeople > 1) {
+      setNumberOfPeople(numberOfPeople - 1);
+    }
+  };
+
+  const handleIncrease = () => {
+    if (numberOfPeople < 6) {
+      setNumberOfPeople(numberOfPeople + 1);
+    }
+  };
+
   return (
     <>
       <div className="flex gap-2 items-center">
         {/* 지역 선택 */}
-        <div className="flex items-center flex-grow border bg-white rounded-md p-3 max-h-11">
+        <div className="flex items-center w-[33%] border bg-white rounded-md p-3 max-h-11">
           <RiMapPinLine />
           <SearchRegion arr={RegionList} />
         </div>
 
         {/* 날짜 선택 */}
-        <div className="flex items-center flex-grow border bg-white rounded-md p-3 max-h-11">
+        <div className="flex items-center w-[33%] border bg-white rounded-md p-3 max-h-11">
           <FaRegCalendarAlt />
           <select className="rounded-md ml-2 outline-none text-sm">
             <option>날짜 선택하기</option>
@@ -24,11 +40,22 @@ const SearchBar = () => {
         </div>
 
         {/* 인원 선택 */}
-        <div className="flex items-center flex-grow border bg-white rounded-md p-3 max-h-11">
+        <div className="flex items-center w-[33%] border bg-white rounded-md p-3 max-h-11">
           <MdOutlinePersonOutline />
-          <select className="rounded-md ml-2 outline-none text-sm">
-            <option>인원 선택하기</option>
-          </select>
+          <div className="flex items-center text-sm">
+            <p className="px-2">인원 선택</p>
+            <div className="flex items-center">
+              <AiOutlineMinusCircle
+                onClick={handleDecrease}
+                className="text-MAIN_GREEN cursor-pointer"
+              />
+              <p className="px-3">{numberOfPeople}</p>
+              <AiOutlinePlusCircle
+                onClick={handleIncrease}
+                className="text-MAIN_GREEN cursor-pointer"
+              />
+            </div>
+          </div>
         </div>
       </div>
 
