8845c3782bbd40c48948c1ce6019aea554fa5ec1
cheesecat47 <cheesecat47@gmail.com>
2024-05-06 16:10:06 +0900

    [BE] chore #43: 사용하지 않는 어노테이션 삭제
    - IntelliJ에서 개발하면서 Jetbrains 어노테이션이 잘못 들어감.

--- backend/src/main/java/com/d106/campu/campsite/repository/jpa/CampsiteRepository.java
+++ backend/src/main/java/com/d106/campu/campsite/repository/jpa/CampsiteRepository.java
@@ -2,7 +2,6 @@ package com.d106.campu.campsite.repository.jpa;
 
 import com.d106.campu.campsite.domain.jpa.Campsite;
 import java.util.Optional;
-import org.jetbrains.annotations.NotNull;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -13,7 +12,6 @@ public interface CampsiteRepository extends JpaRepository<Campsite, Long> {
 
     Optional<Campsite> findByFacltNm(String name);
 
-    @NotNull
     @Query("""
         SELECT c
         FROM campsite c
