d1335fde12e4bbed2aea49b1006aea5b6ec65088
최민경 <kjjpyckjs@gmail.com>
2024-05-08 14:09:40 +0900

    [FE] fix #146: 충돌 해결

--- frontend/src/components/@common/Calendar/CalendarSubmit.tsx
+++ frontend/src/components/@common/Calendar/CalendarSubmit.tsx
@@ -1,16 +1,11 @@
 import { useMemo } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { RootState } from "@/app/store";
 import { formatSimpleDate } from "@/utils/formatDateTime";
 import { diffDays } from "@/utils/diffDays";
 import Button from "@/components/@common/Button/Button";
-import {
-  setEndDate,
-  setStartDate,
-} from "@/features/reservation/campingDateSlice";
 
 const CalendarSubmit = () => {
-  const dispatch = useDispatch();
   const { startDate, endDate } = useSelector(
     (state: RootState) => state.campingDate
   );
@@ -33,12 +28,6 @@ const CalendarSubmit = () => {
     }
   }, [startDate, endDate]);
 
-  // 스토어에 날짜 저장
-  const submitToStore = () => {
-    dispatch(setStartDate(startDate));
-    dispatch(setEndDate(endDate));
-  };
-
   return (
     <div>
       <Button
@@ -49,7 +38,6 @@ const CalendarSubmit = () => {
           selectedDateText ? selectedDateText : "날짜를 선택해주세요."
         }`}
         disabled={isDisable}
-        onClick={submitToStore}
       ></Button>
     </div>
   );
