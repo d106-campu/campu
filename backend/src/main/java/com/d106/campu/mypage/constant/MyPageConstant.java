7db1f84eeebf4a3a0baefb66d16388dd0eea0c70
서준호 <twg0995@naver.com>
2024-05-08 17:25:52 +0900

    [BE] feat #170: 마이페이지 내가 찜한 캠핑장 조회 API 구현

--- backend/src/main/java/com/d106/campu/mypage/constant/MyPageConstant.java
+++ backend/src/main/java/com/d106/campu/mypage/constant/MyPageConstant.java
@@ -4,6 +4,7 @@ public class MyPageConstant {
 
     public static final String RESERVATION_LIST = "reservationList";
     public static final String REVIEW_LIST = "reviewList";
+    public static final String CAMPSITE_LIST = "campsiteList";
     public static final String RESERVATION_CANCLE = "cancle";
     public static final String RESERVATION = "reservation";
     public static final String REVIEW = "review";
