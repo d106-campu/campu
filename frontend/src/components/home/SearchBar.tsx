e63f9611d49e7621abcfa73bd06278ead63b09be
최민경 <kjjpyckjs@gmail.com>
2024-05-08 14:52:02 +0900

    [FE] fix #146: build error 해결

--- frontend/src/components/home/SearchBar.tsx
+++ frontend/src/components/home/SearchBar.tsx
@@ -8,12 +8,11 @@ import { useEffect, useState } from "react";
 import { AiOutlineMinusCircle, AiOutlinePlusCircle } from "react-icons/ai";
 import { IoIosArrowDown } from "react-icons/io";
 import Calendar from "@/components/@common/Calendar/Calendar";
-import { FaArrowRotateRight } from "react-icons/fa6";
-import CalendarSubmit from "../@common/Calendar/CalendarSubmit";
 import { useDispatch, useSelector } from "react-redux";
 import { setKeyword, setPeople } from "@/features/search/searchBarSlice";
 import { useNavigate } from "react-router-dom";
 import { RootState } from "@/app/store";
+import { formatSimpleDate } from "@/utils/formatDateTime";
 
 const SearchBar = () => {
   const [numberOfPeople, setNumberOfPeople] = useState(2);
@@ -22,10 +21,9 @@ const SearchBar = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const searchBarState = useSelector((state: RootState) => state.searchBar);
-  const startDate = useSelector(
-    (state: RootState) => state.searchBar.startDate
+  const { startDate, endDate } = useSelector(
+    (state: RootState) => state.campingDate
   );
-  const endDate = useSelector((state: RootState) => state.searchBar.endDate);
 
   // 스토어 저장 확인용
   // @TODO: API 호출 후, 스토어 비우는 로직 추가해야함
@@ -68,29 +66,24 @@ const SearchBar = () => {
         {/* 날짜 선택 */}
         <div className="relative w-[33%]">
           <div
-            className="flex items-center border bg-white rounded-md p-3 max-h-11"
+            className="flex items-center border bg-white rounded-md p-3 max-h-11 whitespace-nowrap"
             onClick={() => setShowCalendar(!showCalendar)}
           >
             <FaRegCalendarAlt />
             <div className="flex items-center w-full cursor-pointer text-sm px-2">
               <p className="pr-2">
                 {startDate && endDate
-                  ? `${startDate} ~ ${endDate}`
+                  ? `${formatSimpleDate(startDate)} - ${formatSimpleDate(
+                      endDate
+                    )}`
                   : "날짜 선택"}
               </p>
               <IoIosArrowDown />
             </div>
           </div>
           {showCalendar && (
-            <div className="absolute top-full left-0 bg-white rounded-md z-20 mt-1 w-[400px] border pb-4">
+            <div className="absolute top-full left-0 bg-white rounded-md z-20 mt-1 w-[400px] border pb-8">
               <Calendar />
-              <div className="px-8">
-                <button className="flex items-center gap-2 cursor-pointer p-2">
-                  <FaArrowRotateRight color="C9C9C9" />
-                  <span className="text-GRAY">일정 초기화</span>
-                </button>
-                <CalendarSubmit></CalendarSubmit>
-              </div>
             </div>
           )}
         </div>
