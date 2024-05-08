f4b7eb8fb890fca74492d5aa023f40d1dad188fa
최민경 <kjjpyckjs@gmail.com>
2024-05-08 22:27:39 +0900

    [FE] feat #178: 캠핑장 조회 API 타입 지정

--- frontend/src/types/search.ts
+++ frontend/src/types/search.ts
@@ -1,3 +1,44 @@
+export interface ILocationSimpleRes {
+  mapX: number;
+  mapY: number;
+}
+
+export interface ICampsiteSimpleRes {
+  id: number;
+  facltNm: string;
+  lineIntro: string;
+  doNm: string;
+  sigunguNm: string;
+  addr1: string;
+  addr2: string | null;
+  thumbnailImageUrl: string;
+  campsiteLocation: ILocationSimpleRes;
+  like: boolean;
+  available: boolean;
+}
+
+// 캠핑장 조회 Response (data.campsiteList.content)
+export interface ICampsiteListRes {
+  content: ICampsiteSimpleRes[];
+}
+
+export interface IPageableSimpleReq  {
+  page : number;
+  size: number;
+}
+
+// 캠핑장 조회 Request 
+export interface ICampsiteReq {
+  doNm ?: string;
+  sigunguNm  ?: string;
+  startDate : string;
+  endDate : string;
+  induty ?: string;
+  theme ?: string;
+  pageable : IPageableSimpleReq
+}
+
+// 더미데이터 Type
 export interface ICampingGround {
   id: number;
   facltNm: string;
