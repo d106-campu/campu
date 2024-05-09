d9c8859a42445835e7f33088690cc1da5bd5556c
Danbi <silverwill39@gmail.com>
2024-05-09 15:29:31 +0900

    [FE] feat #193: 무한 스크롤을 위한 커스텀 훅 구현

--- frontend/src/types/reservation.ts
+++ frontend/src/types/reservation.ts
@@ -1,8 +1,41 @@
+import { IPageable, ISort } from "./model";
+
 // 좋아요
 export interface ILike {
-    like: boolean;
-  }
+  like: boolean;
+}
 
 export interface ILikeRes {
   likeResponse: ILike;
+}
+
+// 방 목록 아이템
+export interface IRoomItem {
+  id: number;
+  induty: string;
+  name: string;
+  baseNo: number;
+  maxNo: number;
+  price: number;
+  extraPrice: number;
+  roomCnt: number;
+  toiletCnt: number;
+  supplyList: string[] | null;
+}
+
+// 방 목록
+export interface IRoomListRes {
+  roomList: {
+    content: IRoomItem[];
+    pageable: IPageable;
+    last: boolean;
+    totalPages: number;
+    totalElements: number;
+    first: boolean;
+    size: number;
+    number: number;
+    sort: ISort;
+    numberOfElements: number;
+    empty: boolean;
+  };
 }
