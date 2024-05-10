fe1df27b3c672f5414a354e422ae69c4788c47fa
최민경 <kjjpyckjs@gmail.com>
2024-05-10 15:41:17 +0900

    [FE] feat #217: 검색바 결과에 따른 캠핑장 조회 API 통신

--- frontend/src/components/search/searchSection/SearchSection.tsx
+++ frontend/src/components/search/searchSection/SearchSection.tsx
@@ -1,13 +1,36 @@
 import SearchBar from "@/components/home/SearchBar";
 import { ICampingGround } from "@/types/search";
 import SearchCampingItem from "./SearchCampingItem";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { addCampingData } from "@/features/search/campingMapSlice";
 import { useEffect } from "react";
 import SearchNoResult from "./SearchNoResult";
+import { useCampsite } from "@/hooks/search/useCampsite";
+import { RootState } from "@/app/store";
+import { dayOfWeekend } from "@/utils/dayOfWeekend";
 
 const SearchSection = () => {
+  const { useCampsiteList } = useCampsite();
   const dispatch = useDispatch();
+  const searchBarState = useSelector((state: RootState) => state.searchBar);
+
+  // 디폴트 값들 지정
+  const weekendDates = dayOfWeekend();
+  const startday = searchBarState.startDate || weekendDates.saturday;
+  const endday = searchBarState.endDate || weekendDates.sunday;
+  const headcnt = searchBarState.numberOfPeople || 2;
+
+  //  캠핑장 리스트 조회
+  const { data: campsiteOfSearch } = useCampsiteList({
+    doNm: searchBarState.region || undefined,
+    sigunguNm: searchBarState.subRegion || undefined,
+    startDate: startday,
+    endDate: endday,
+    headCnt: headcnt,
+    pageable: { page: 0, size: 1000 },
+  });
+
+  console.log(campsiteOfSearch?.data.campsiteList);
 
   const addDummyDataToStore = () => {
     dispatch(addCampingData(dummy));
