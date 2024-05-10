2aac7a6f656c39482ae1f292c334d89203e261c6
최민경 <kjjpyckjs@gmail.com>
2024-05-10 17:24:38 +0900

    [FE] feat #217: 검색 페이지를 벗어났을 때 스토어를 지우는 코드 추가

--- frontend/src/pages/search/index.tsx
+++ frontend/src/pages/search/index.tsx
@@ -1,8 +1,18 @@
-
 import Header from "@/components/@common/Header/Header";
 import SearchContainer from "@/components/search/SearchContainer";
+import { clearCampingData } from "@/features/search/campingMapSlice";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 
 const SearchPage = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    return () => {
+      dispatch(clearCampingData());
+    };
+  }, [dispatch]);
+
   return (
     <>
       <Header page={"search"} />
