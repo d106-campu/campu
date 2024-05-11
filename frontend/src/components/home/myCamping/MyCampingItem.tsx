a78805afd074c1041aa9a1c6ad7a84462936c56e
최민경 <kjjpyckjs@gmail.com>
2024-05-11 20:15:38 +0900

    [FE] feat #249: 메인페이지 내가 찜한 캠핑장 API 통신

--- frontend/src/components/home/myCamping/MyCampingItem.tsx
+++ frontend/src/components/home/myCamping/MyCampingItem.tsx
@@ -1,33 +1,35 @@
 import LikeButton from "@/components/@common/Like/LikeButton";
-import { ICampsiteSimpleRes } from "@/types/search";
+import { IMyFavoritCampRes } from "@/types/myFavorite";
 import { FaStar } from "react-icons/fa6";
 
-const MyCampingItem = ({ camping }: { camping: ICampsiteSimpleRes }) => {
+const MyCampingItem = ({ camping }: { camping: IMyFavoritCampRes }) => {
   return (
-    <div key={camping.id} className="px-2 py-5 w-[25%] relative">
+    <div key={camping.campsiteId} className="px-2 py-5 w-[25%] relative">
       <img
         src={camping.thumbnailImageUrl}
-        alt={camping.facltNm}
+        alt={camping.campsiteName}
         className="w-full rounded-md h-32 object-cover object-center"
       />
       <div className="absolute top-7 right-4">
-        <LikeButton campsiteId={camping.id} iconSize={25} />
+        <LikeButton campsiteId={camping.campsiteId} iconSize={25} />
       </div>
       <div className="p-2">
         <div className="flex justify-between">
-          <h1 className="font-bold">{camping.facltNm}</h1>
+          <h1 className="font-bold">{camping.campsiteName}</h1>
           <div className="flex items-center">
             <FaStar className="text-yellow-500 mx-1" />
             <p>{camping.score.toFixed(1)}</p>
           </div>
         </div>
         <p className="text-xl text-orange-700 font-extrabold">
-          {camping.price.toLocaleString("ko-KR")}원 ~
+          {camping.minPrice.toLocaleString("ko-KR")}원 ~
         </p>
         <p className="text-sm overflow-hidden whitespace-nowrap overflow-ellipsis">
           {camping.lineIntro}
         </p>
-        <p className="text-sm text-gray-400">{camping.addr1}</p>
+        <p className="text-sm text-gray-400 overflow-hidden whitespace-nowrap overflow-ellipsis">
+          {camping.address}
+        </p>
       </div>
     </div>
   );
