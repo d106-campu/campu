a8b1169ec1f0a5ae43b03cd59620d71fe15bf52d
최민경 <kjjpyckjs@gmail.com>
2024-05-13 19:56:59 +0900

    [FE] feat #302: 사장님페이지에서 전체 캠핑장 조회 및 사이드바 적용

--- frontend/src/hooks/owner/useOwner.ts
+++ frontend/src/hooks/owner/useOwner.ts
@@ -3,12 +3,11 @@ import { IOwnerCampsiteReq } from "@/types/owner";
 import { useQuery } from "@tanstack/react-query";
 
 export const useOwner = () => {
-
-    const useGetOwnerCampsiteList = (props: IOwnerCampsiteReq) => {
-        return useQuery({
-            queryKey: ['ownerCampsite', props],
-            queryFn: () => getOwnerCampsiteList(props),
-        })
-    }
-  return {useGetOwnerCampsiteList};
+  const useGetOwnerCampsiteList = (props: IOwnerCampsiteReq) => {
+    return useQuery({
+      queryKey: ["ownerCampsite", props],
+      queryFn: () => getOwnerCampsiteList(props),
+    });
+  };
+  return { useGetOwnerCampsiteList };
 };
