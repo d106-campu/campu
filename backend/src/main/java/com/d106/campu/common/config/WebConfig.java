793582db3d142e748459a3d84d8f3a2856f3e1b3
서준호 <twg0995@naver.com>
2024-05-13 10:52:20 +0900

    [BE] feat #250: octet-stream converter 추가

--- backend/src/main/java/com/d106/campu/common/config/WebConfig.java
+++ backend/src/main/java/com/d106/campu/common/config/WebConfig.java
@@ -1,15 +1,22 @@
 package com.d106.campu.common.config;
 
+import com.d106.campu.common.util.OctetStreamMessageConverter;
 import com.d106.campu.image.constant.ImageConstant;
 import java.nio.file.Path;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.CacheControl;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@RequiredArgsConstructor
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    private final OctetStreamMessageConverter octetStreamMessageConverter;
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
@@ -19,6 +26,11 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addResourceHandler("/file/user/**", "/file/campsite/**")
             .addResourceLocations(getResourceLocation(ImageConstant.USER_DIR), getResourceLocation(ImageConstant.CAMPSITE_DIR))
             .setCacheControl(CacheControl.maxAge(3, TimeUnit.MINUTES));
+    }
+    
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.add(octetStreamMessageConverter);
     }
 
     private String getResourceLocation(Path dir) {
