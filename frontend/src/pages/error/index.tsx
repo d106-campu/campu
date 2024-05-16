c35e7e0e5e7949a306958cbe4af1d9d787a096ef
최민경 <kjjpyckjs@gmail.com>
2024-05-16 20:49:37 +0900

    [FE] feat #376: 에러페이지 구현

--- frontend/src/pages/error/index.tsx
+++ frontend/src/pages/error/index.tsx
@@ -1,4 +1,22 @@
+import { errorOptions } from "@/assets/lotties/lottieOptions";
+import Lottie from "react-lottie";
+import { useNavigate } from "react-router-dom";
+
 const ErrorPage = () => {
-  return <div></div>;
+  const navigate = useNavigate();
+  return (
+    <div className="w-screen h-screen flex flex-col justify-center items-center">
+      <Lottie options={errorOptions} width={460} height={400} />
+      <p className="pb-4">잘못된 경로입니다 😅</p>
+      <div>
+        <button
+          onClick={() => navigate("/")}
+          className="border border-blue-400 px-4 py-2 rounded-lg text-sm text-blue-500"
+        >
+          메인으로
+        </button>
+      </div>
+    </div>
+  );
 };
 export default ErrorPage;
