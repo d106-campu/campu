5057d70a1c343c3e3d7900502bd916dae2b03a98
Danbi <silverwill39@gmail.com>
2024-05-17 16:58:39 +0900

    [FE] fix #401: 리뷰 UI 에러 수정

--- frontend/src/components/reservation/SimpleReviewItem.tsx
+++ frontend/src/components/reservation/SimpleReviewItem.tsx
@@ -4,7 +4,7 @@ import { formatSimpleReviewTime } from "@/utils/formatDateTime";
 const SimpleReviewItem = ({ review }: { review: IReview }) => {
   return (
     <div className="flex flex-col rounded-lg bg-SUB_GREEN_01 p-4 w-[33%] text-SUB_BLACK text-sm">
-      <div className="line-clamp-2 h-[30px]">{review.content}</div>
+      <div className="line-clamp-2 h-[30px] text-xs">{review.content}</div>
       <div className="pt-2 text-end mt-auto">
         <p className="font-bold text-end">{review.user.nickname}</p>
         <p className="text-xs text-[#919191]">
