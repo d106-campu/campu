0f87aa930cc2927a7c39f6587f8e0a0464bba917
최민경 <kjjpyckjs@gmail.com>
2024-05-18 17:39:03 +0900

    [FE] feat #408: 메인페이지 상세보기 경로 추가

--- frontend/src/components/home/myCamping/MyCampingItem.tsx
+++ frontend/src/components/home/myCamping/MyCampingItem.tsx
@@ -1,10 +1,23 @@
 import LikeButton from "@/components/@common/Like/LikeButton";
 import { IMyFavoritCampRes } from "@/types/my";
+import { scrollToTop } from "@/utils/scrollToTop";
 import { FaStar } from "react-icons/fa6";
+import { useNavigate } from "react-router-dom";
 
 const MyCampingItem = ({ camping }: { camping: IMyFavoritCampRes }) => {
+  const navigate = useNavigate();
+
+  const goToDetail = () => {
+    navigate(`/camps/${camping.campsiteId}`);
+    scrollToTop();
+  };
+
   return (
-    <div key={camping.campsiteId} className="px-2 py-5 w-[25%] relative">
+    <div
+      onClick={goToDetail}
+      key={camping.campsiteId}
+      className="px-2 py-5 w-[25%] relative"
+    >
       <img
         src={camping.thumbnailImageUrl}
         alt={camping.campsiteName}
